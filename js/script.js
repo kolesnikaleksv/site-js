c7dd5381a1a89d60da52911ba463dc1e38eb2217
kolesnikaleks <kolesnikaleksv@gmail.com>
2020-01-21 10:34:33 +0200

    site JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".slider").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 8000,
+  });
+  $(".btn-nav").on("click", function() {
+    var target = $ (this).data("target");
+    $(target).toggleClass("nav__list--open");
+  });
+});
---
+++ site.js
@@ -0,0 +1,14 @@
+@import url("css/normalize.css");
+
+*,
+*::after,
+*::before {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.clearfix::after {
+    content: "";
+    display: table;
+    clear: both;
+}
